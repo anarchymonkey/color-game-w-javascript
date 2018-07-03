4e6e5b05f07212dec7872efd5ed6952dc7478358
Aniket-Batabyal <Aniketbatabyal1996@gmail.com>
2018-07-03 12:56:02 +0530

    added raised difficulty

--- javascript/index.js
+++ javascript/index.js
@@ -1,18 +1,11 @@
-var colors = [
-  'rgb(255, 0, 0)',
-  'rgb(255, 255, 0)',
-  'rgb(0, 255, 255)',
-  'rgb(0, 255, 0)',
-  'rgb(0, 0, 255)',
-  'rgb(255, 0, 255)']
+var colors = generateRandomColors(23);
   var boxes = document.querySelectorAll(".square")
-
-  var ans = colors[3]
   var printRGB = document.querySelector("#id1")
   var message = document.querySelector("#me")
   var jumbotron = document.querySelector(".new")
   var flag = 0;
   var gameOver = false
+  var ans = pickedColor();
 
   printRGB.textContent = ans;
 
@@ -44,4 +37,40 @@ var colors = [
       boxes[i].style.backgroundColor = color;
     jumbotron.style.backgroundColor = color;
     }
+  }
+
+  function pickedColor(){
+  var random =   Math.floor(Math.random() * colors.length)
+
+  return colors[random]
+  }
+
+  function randomColor()
+  {
+    //pick a red from 0 - 255
+    //green 0-255
+    //blue 0-255
+
+    var red = Math.floor(Math.random() *257)
+    var green = Math.floor(Math.random() *257)
+    var blue = Math.floor(Math.random() *257)
+
+    //rgb(red,greem,blue)
+
+    return "rgb("+ red + ", " + green + ", " + blue + ")";
+  }
+
+  function generateRandomColors(num)
+  {
+    //make an array
+    //add num random colors to array
+
+    var arr = []
+//Repeat num times;
+    for(var i = 0; i< num ;i++)
+    {
+      //get the random color
+      arr.push(randomColor())
+    }
+    return arr;
   }
